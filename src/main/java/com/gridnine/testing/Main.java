6ada881d623849266433a44c78f0b97bef01c2b6
yarek <yarek2003@mail.ru>
2024-06-26 20:19:54 +0300

    Done

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -7,10 +7,17 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Полный список перелетов");
         System.out.println(FlightBuilder.createFlights());
+        System.out.println("--------------------------");
+        System.out.println("Вылет до текущего момента времени");
         System.out.println(excludeDeparturesBeforeCurrentTime(FlightBuilder.createFlights()));
-      //  System.out.println(secondFilter(FlightBuilder.createFlights()));
-     //  System.out.println(thirdFilter(FlightBuilder.createFlights()));
+        System.out.println("--------------------------");
+        System.out.println("Сегменты с датой прилета раньше даты вылета");
+        System.out.println(excludeArrivalBeforeDeparture(FlightBuilder.createFlights()));
+        System.out.println("--------------------------");
+        System.out.println("Перелеты, где общее время, проведенное на земле, превышает два часа");
+        System.out.println(excludeTimeOnLandMoreTwoHours(FlightBuilder.createFlights()));
     }
 
 
